b393f4356aed4c3db8850b357f4f89a6810bd3e7
Harnoor Singh <hsingh9@student.gsu.edu>
2018-01-28 10:39:22 -0500

    SignUp Avatar completed

--- app/src/main/java/com/iharnoor/slackclone/SignUpActivity.kt
+++ app/src/main/java/com/iharnoor/slackclone/SignUpActivity.kt
@@ -1,22 +1,44 @@
 package com.iharnoor.slackclone
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
-
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
 
-    fun generateBackgroundClicked(view: View) {
+    fun avatarImgClicked(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if (color == 0)
+            userAvatar = "light$avatar"
+        else
+            userAvatar = "dark$avatar"
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        avatarImgSignup.setImageResource(resourceId)
     }
 
-    fun avatarImgClicked(view: View) {
+    fun generateBackgroundClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        avatarImgSignup.setBackgroundColor(Color.rgb(r, g, b))
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+        avatarColor = "[$savedR, $savedG, $savedB]"
     }
 
     fun createUserClicked(view: View) {
